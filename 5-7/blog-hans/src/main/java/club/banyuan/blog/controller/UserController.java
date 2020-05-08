9589392bd81b85b23cc5a31c0a90931a99706951
Your Name <you@example.com>
2020-05-08 14:57:52 +0800

    5-7分页功能补充

--- 5-7/blog-hans/src/main/java/club/banyuan/blog/controller/UserController.java
+++ 5-7/blog-hans/src/main/java/club/banyuan/blog/controller/UserController.java
@@ -19,21 +19,45 @@ public class UserController {
         User user = new UserService().findByName("zhangsan");
         List list = new BlogService().findBlogs(user);
         List list1 = new ArrayList();
-        for(int i=0;i<size;i++){
-            list1.add(list.get(page*size+-size+i));
+
+//        小于边界范围，返回第一页
+        if(page<=0||size<=0){
+            if(size>0&&size<list.size()){
+                page = 1;
+                list1 = pages(page,size,list);
+            }else{
+                list1.add("size必须大于0且不能大于100");
+            }
+            return list1;
+        }
+
+//        都在范围内，正常输出
+        list1 = pages(page,size,list);
+
+//        超出边界范围，返回最后一页
+        if(list1.size()==0){
+            int number ;
+            if(list.size()%size==0){
+                number = list.size()/size;
+            }else{
+                number = list.size()/size+1;
+            }
+            page = number;
+            list1 = pages(page,size,list);
         }
         return list1;
     }
 
-//    @GetMapping
-//    @ResponseBody
-//    public List<Blog> requestP(@RequestParam("page") Integer page,@RequestParam("size") Integer size){
-//        User user = new UserService().findByName("zhangsan");
-//        List list = new BlogService().findBlogs(user);
-//        List list1 = new ArrayList();
-//        for(int i=1;i<=size;i++){
-//            list1.add(page*size-size+i);
-//        }
-//        return list1;
-//    }
+//    计算分页的算法
+    public List pages(Integer pages,Integer size,List list){
+        List list2 = new ArrayList();
+        for(int i=0;i<size;i++){
+            if(pages*size-size+i>=list.size()){
+                break;
+            }else{
+                list2.add(list.get(pages*size-size+i));
+            }
+        }
+        return list2;
+    }
 }
