dd748cec98d4bd6ea45790de2addb2c89fe9f1ca
Your Name <you@example.com>
2020-05-13 18:55:09 +0800

    力扣练习题

--- lianxi/src/Test.java
+++ lianxi/src/Test.java
@@ -1,13 +1,20 @@
  class Test {
     public static void main(String[] args) {
-        System.out.println("1");
         Likou likou = new Likou();
         int number[] = {1,0,0,0,1,0,0};
-
+//        int number1[] = {1,2,3,4,5};
+        int number1[] = {3,4,2,3};
+        int number2[] = {2,4};
 //        System.out.println(likou.isPalindrome(122521));
-//        System.out.println(likou.kuo("{}"));
+//        System.out.println(likou.kuo("{ }"));
 //        System.out.println(likou.shu(number,2));
-        System.out.println(likou.hua(number,2));
-        System.out.println("123");
+//        System.out.println(likou.hua(number,2));
+//        System.out.println(likou.gong(number1,number2));
+//        System.out.println(likou.reverseLeftWords("lrloseumgh",6));
+//        System.out.println(likou.buddyStrings("abab","abab"));
+//        System.out.println(likou.lengthOfLastWord(" "));
+        System.out.println(likou.checkPossibility(number1));
+
+
     }
 }
